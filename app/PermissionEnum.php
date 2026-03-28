97418a572eda39c06e7476feea2679fb20d7661b
Aron <aron.papp2003@gmail.com>
2026-03-28 12:03:25 +0100

    update

--- app/PermissionEnum.php
+++ app/PermissionEnum.php
@@ -5,6 +5,15 @@ namespace App;
 enum PermissionEnum: string
 {
     case ADD_USERS_TO_GUILD = 'add_users_to_guild';
+    case DELETE_USERS_FROM_GUILD = 'delete_users_from_guild';
+    case EDIT_GUILD_USERS = 'edit_guild_users';
+    case EDIT_GUILD_SETTINGS = 'edit_guild_settings';
+    case EDIT_GUILD_USERS_DUTIES = 'edit_guild_users_duties';
+    case VIEW_GUILD_USERS = 'view_guild_users';
+    case VIEW_GUILD_SETTINGS = 'view_guild_settings';
+    case VIEW_GUILD_DUTIES_STATS = 'view_guild_duties_stats';
+
+
 
     /**
      * @return array
