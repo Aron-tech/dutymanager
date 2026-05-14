a99a45971e8f8c7fdb9a94de56baefa8bcb9a3c5
Aron <aron.papp2003@gmail.com>
2026-05-14 20:00:12 +0200

    update

--- routes/console.php
+++ routes/console.php
@@ -2,4 +2,6 @@
 
 Schedule::command('app:cancel-invalid-active-duties-command')->everySixHours();
 Schedule::command('app:clean-up-command')->daily();
+Schedule::command('app:handle-expired-holiday-command')->daily();
+Schedule::command('app:handle-expired-punishment-command')->daily();
 
