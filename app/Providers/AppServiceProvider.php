1c88fce0ba2ede303478e110983f7ecb753b8b12
Aron <aron.papp2003@gmail.com>
2026-05-16 18:00:51 +0200

    update

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,10 +2,20 @@
 
 namespace App\Providers;
 
-use Illuminate\Support\ServiceProvider;
-use Laravel\Cashier\Cashier;
+use App\Models\Duty;
+use App\Models\Guild;
+use App\Models\GuildUser;
+use App\Models\Image;
+use App\Models\Item;
 use App\Models\Subscription;
 use App\Models\User;
+use App\Observers\DutyObserver;
+use App\Observers\GuildObserver;
+use App\Observers\GuildUserObserver;
+use App\Observers\ImageObserver;
+use App\Observers\ItemObserver;
+use Illuminate\Support\ServiceProvider;
+use Laravel\Cashier\Cashier;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -22,6 +32,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        GuildUser::observe(GuildUserObserver::class);
+        Duty::observe(DutyObserver::class);
+        Image::observe(ImageObserver::class);
+        Guild::observe(GuildObserver::class);
+        Item::observe(ItemObserver::class);
         Cashier::useSubscriptionModel(Subscription::class);
         Cashier::useCustomerModel(User::class);
     }
